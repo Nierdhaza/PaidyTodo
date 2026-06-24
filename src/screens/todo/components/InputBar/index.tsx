d0ee4e4ceb18a68fcd701db040beabbef1a8b82f
volodymyr.kachmar <volodymyr_kachmar@icloud.com>
2026-06-24 18:27:42 +0200

    feat: add input offset with keyboard avoiding view

--- src/screens/todo/components/InputBar/index.tsx
+++ src/screens/todo/components/InputBar/index.tsx
@@ -32,28 +32,26 @@ export default function InputBar() {
 
   return (
     // TODO: test on real device because KeyboardAvoidingView doesn't work on simulator
-    // <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-    <View style={styles.inputWrap}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter here"
-        placeholderTextColor="#D1D5DB"
-        value={text}
-        onChangeText={handleChangeText}
-        onSubmitEditing={handleSubmit}
-        returnKeyType="done"
-      />
-      <Pressable onPress={handleSubmit} style={styles.addBtn} accessibilityRole="button">
-        <Text style={styles.addText}>{selectedTodo ? 'UPDATE' : 'ADD'}</Text>
-      </Pressable>
-    </View>
-    // </KeyboardAvoidingView>
+    <KeyboardAvoidingView keyboardVerticalOffset={80} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <View style={styles.inputWrap}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter here"
+          placeholderTextColor="#D1D5DB"
+          value={text}
+          onChangeText={handleChangeText}
+          onSubmitEditing={handleSubmit}
+          returnKeyType="done"
+        />
+        <Pressable onPress={handleSubmit} style={styles.addBtn} accessibilityRole="button">
+          <Text style={styles.addText}>{selectedTodo ? 'UPDATE' : 'ADD'}</Text>
+        </Pressable>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 /**
- * 3) write unit tests
  * 4) try to write button component using Pressable
  * 5) extend theme file with paddings (s, xs, m, l, xl), margins (s, xs, m, l, xl), borderRadius (s, xs, m, l, xl)
- * 2) test keyboard avoiding view on real device
  */
